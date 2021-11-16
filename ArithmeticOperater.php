3b434abe7da3f3ba3d43b39a660a18d1298216ea
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2021-11-16 12:33:12 +0530

    Add files via upload

---
+++ ArithmeticOperater.php
@@ -0,0 +1,17 @@
+<?php 
+ $a=10;
+ $b=5;
+ $c=$a + $b;
+ $d=$a - $b;
+ $e=$a * $b;
+ $f=$a ** $b;
+ $g=$a / $b;
+ $h=$a % $b;
+ echo $c . "<br>";
+ echo $d . "<br>";
+ echo $e . "<br>";
+ echo $f . "<br>";
+ echo $g . "<br>";
+ echo $h . "<br>";
+//  echo $c  . $d . $e . $f . $g . $h ;
+?>
