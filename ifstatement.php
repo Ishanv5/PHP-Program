eb69e11d9c0234b5a7dc222ac8d5b8514d5da3af
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2021-12-13 11:21:28 +0530

    Add files via upload

---
+++ ifstatement.php
@@ -0,0 +1,12 @@
+<?php
+  $a=3;
+  $b=10;
+  if($a<$b)
+  {
+     echo "A is Smaller<br> ";
+  }
+  echo "Outside the if statement";
+//   if($a>$b):  Another if statement Syntax
+//     echo "B is Greater";
+//   endif;
+?>
