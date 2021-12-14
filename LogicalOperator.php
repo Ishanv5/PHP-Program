f0b4011ee9804f3bb80646ff236c986809e4e4fb
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2021-12-14 10:16:42 +0530

    Add files via upload

---
+++ LogicalOperator.php
@@ -0,0 +1,19 @@
+<?php 
+  $age=22;
+//   if($age>=18 && $age<=21)
+//   {
+//       echo "You are eligible......";
+//   }
+//   if($age>=18 || $age<=21)
+//   {
+//       echo "You are eligible......";
+//   }
+//   if(!($age<=18))
+//   {
+//       echo "You are eligible......";
+//   }
+  if($age>=18 xor $age<=21)
+  {
+      echo "You are eligible......";
+  }
+?>
