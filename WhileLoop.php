37c0de704576590aaeddca9b529f0a3e36826a67
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2021-12-14 18:35:59 +0530

    Add files via upload

---
+++ WhileLoop.php
@@ -0,0 +1,15 @@
+<?php
+ $a=1;
+ echo "<ul>";
+ while($a<=5)
+ {
+     echo $a . ")  Hello This is Ishan Vaghela  Nice to meet you..... \n";
+        $a++;//    $a=$a+1;
+ }
+ while($a>=1)
+ {
+     echo $a . ")  Hello This is Ishan Vaghela  Nice to meet you..... \n";
+        $a--;   //$a=$a-1;
+ }
+ echo "</ul>";
+?>
