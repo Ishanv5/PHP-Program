36bd36f659c8e305de3714eecd616b1be06d0981
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2021-12-17 19:04:44 +0530

    Add files via upload

---
+++ Functions.php
@@ -0,0 +1,18 @@
+<?php
+   function hello(){
+       echo "Hello from ishan vaghela nice to meet you \n ";
+   }
+   function software(){
+       echo "Hello from ishan software \n ";
+   }
+
+   hello();
+   hello();
+   hello();
+   echo "this is an example \n";
+   software();
+   software();
+   software();
+   hello();
+   hello();
+?>
