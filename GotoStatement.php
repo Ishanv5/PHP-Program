59bf9c4e1f239bf5c64578c5b0f41696886875a2
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2021-12-17 18:55:30 +0530

    Add files via upload

---
+++ GotoStatement.php
@@ -0,0 +1,12 @@
+<?php
+   echo "Hello Ishan VAghela \n";
+   echo "Hello Ishan VAghela \n";
+   goto abc;
+   echo "Hello Ishan VAghela \n";
+   echo "Hello Ishan VAghela \n";
+   echo "Hello Ishan VAghela \n";
+   echo "Hello Ishan VAghela \n";
+    abc:
+    echo "Hey this label abc";
+
+?>
