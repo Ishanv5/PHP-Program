21b3c1768b23e848148a0e32cf4f84e0d23241e7
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2021-12-19 17:46:33 +0530

    Add files via upload

---
+++ FunctionwithParameter.php
@@ -0,0 +1,12 @@
+<?php
+  function hello($fname,$Lname){
+      echo "Hello $fname $Lname\n";
+  }
+  function sum($a,$b){
+      echo  $a + $b;
+  }
+  hello("Ishan","Software");
+  hello("Bill","Gates");
+  hello("James","Goslings");
+  sum(10,20)
+?>
