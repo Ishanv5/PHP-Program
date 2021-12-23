f1878ecc694721c47f4ca06216c1bb7c4dbb493d
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2021-12-23 11:28:22 +0530

    Add files via upload

---
+++ foreachLoop.php
@@ -0,0 +1,14 @@
+<?php
+//   $colors=["red","green","blue"];
+//   foreach($colors as $value){
+//       echo $value . "\n";
+//   }
+$age=[
+    "Ishan"=>18,
+    "Sujal"=>17,
+    "Meet"=>16
+];
+foreach($age as $key=>$value){
+    echo $key . " = " .  $value . "\n";
+}
+?>
