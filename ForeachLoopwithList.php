72a5f3bd06aeeb010922348bb3ca546f2beb5c59
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2021-12-25 11:07:23 +0530

    Add files via upload

---
+++ ForeachLoopwithList.php
@@ -0,0 +1,12 @@
+<?php
+$emp=[
+    [1,"Ishan","IT Engineer",60000],
+    [2,"Sujal","AC CA",100000],
+    [3,"Meet","CA",100000],
+    [4,"Nikita","CAF",12000]
+];
+foreach($emp as list($id,$name,$designation,$salary)){
+ echo "$id  $name  $designation  $salary \n";
+}
+
+?>
