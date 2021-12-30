b33b84a515dfbebbfe17e0ac39a5c248ab6bd410
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2021-12-30 11:31:05 +0530

    Add files via upload

---
+++ ArrayPush.php
@@ -0,0 +1,8 @@
+<?php
+$colors=['orange','red','blue'];
+array_push($colors,"violet");
+// array_pop($colors);
+print_r($colors);
+array_pop($colors);
+print_r($colors);
+?>
---
+++ Unshift.php
@@ -0,0 +1,8 @@
+<?php
+$colors=["red", "blue", "indigo"];
+// array_shift($colors);
+// print_r($colors);
+array_unshift($colors, "Purple", "Yellow");
+print_r($colors);
+
+?>
