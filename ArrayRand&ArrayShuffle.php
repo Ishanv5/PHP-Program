b84231be6bcd77dff9892d42c8f42fe2366803ce
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2022-01-08 11:07:06 +0530

    Add files via upload

---
+++ ArrayRand&ArrayShuffle.php
@@ -0,0 +1,8 @@
+ <?php
+ $colors=array("red","green","blue","yellow");
+//  $newArray=array_rand($colors,2);
+ shuffle($colors);
+//  print_r($newArray);
+ print_r($colors);
+// echo $colors[$newArray];
+ ?>
