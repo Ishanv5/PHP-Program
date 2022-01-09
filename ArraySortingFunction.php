98d86564779ae6d8c88606f622d0737b6e66b20c
Ishan Vaghela <86005959+Ishanv5@users.noreply.github.com>
2022-01-09 11:19:00 +0530

    Add files via upload

---
+++ ArraySortingFunction.php
@@ -0,0 +1,26 @@
+<?php
+// $food=array('Dosa','Pani-puri','Pizza','Burger');
+// $food=array(
+//     "d"=>"lemon",
+//     "a"=>"orange",
+//     "b"=>"banana",
+//     "c"=>"apple"
+// );
+$food=array('Dosa','Pani-puri');
+$newArray=array_reverse($food);
+// $veggie=array('lemon','carrot');
+// array_multisort($food,$veggie);
+ print_r($newArray);
+// print_r($veggie);
+// $array1=array("Img12.png","Img10.png","img2.png","img1.png");
+// sort($food);
+// rsort($food);
+// asort($food);
+// arsort($food);
+// ksort($food);
+// krsort($food);
+// natsort($array1);
+// natcasesort($array1);
+// print_r($array1);
+// print_r($food);
+?>
